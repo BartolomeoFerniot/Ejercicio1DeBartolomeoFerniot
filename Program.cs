086fbe7a4813ea916aa4b99a836d98e08a40c43d
Ferni <Ferni@LAPTOP-D5OFFVLI>
2022-07-05 23:35:33 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio1DeBartolomeoFerniot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double pesos, dolares, euros;
+            Console.Write("Ingrese la cantidad de pesos que desea convertir a dólares y euros: ");
+            pesos = Convert.ToDouble(Console.ReadLine());
+            dolares = pesos / 230;
+            euros = dolares / 1.17;
+            Console.WriteLine($"La cantidad de dólares que puede comprar con {pesos} pesos son: {dolares} dólares");
+            Console.WriteLine($"Y la cantidad de euros que puede comprar con la misma cantidad de pesos son: {euros} euros");
+
+            Console.ReadLine();
+
+        }
+    }
+}
